7b6ddcaa95ef92a79ea8ea654f1c5faa161fe2eb
KrzysztofLiszka <82656683+KrzysztofLiszka@users.noreply.github.com>
2024-04-04 19:12:23 +0200

    added: e2e tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -3,6 +3,7 @@ describe('Swiper Gallery Test', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-slide-active').should('contain', 'London');
   });
 });
 
@@ -14,5 +15,38 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000);
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-slide-active').should('contain', 'France');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if first slide contains "Rome"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks previous slide', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.get('.swiper-slide-active').should('contain', 'France');
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.wait(2000);
   });
 });
